cae5d1f265eaafecd3079c4c03d144884fa4f6e3
Rodney <rodney.07.ramos@gmail.com>
2023-10-18 20:16:28 -0300

    add class disciplina

---
+++ src/entities/Disciplina.js
@@ -0,0 +1,8 @@
+
+export default class Disciplina{
+    constructor(codDisciplina,nome,semestre){
+        this.codDisciplina=codDisciplina;
+        this.nome=nome;
+        this.semestre=semestre;
+}
+}
