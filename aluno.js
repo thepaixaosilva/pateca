f70f6de3c711cb0db6a6b8deeb9c96ebe10f12b1
Victoria Miki Fujii <fujiimikii@gmail.com>
2023-10-20 16:59:46 -0300

    Add método na classe Aluno

--- aluno.js
+++ aluno.js
@@ -4,4 +4,20 @@ export default class Aluno {
         this.nome = nome
         this.senha = senha
     }
+
+    verificarCoordenador(ra, senha) { // método para verificar se o login é do coordenador
+        const minhaPromise = new Promise((resolve,reject) => {
+            if (ra == 123456789 && senha == "coordenador") {
+                resolve(`É coordenador`)
+            } else {
+                reject(`Não é coordenador`)
+            }
+        })
+
+        return minhaPromise.then(
+            (resultado) => console.log(resultado)
+        ).catch(
+            (resultado) => console.log(resultado)
+        )
+    }
 }
