395b81375001fb84718b8d4ea3f5b3c8c8ee3ba2
IsabellaOshima <143272475+IsabellaOshima@users.noreply.github.com>
2023-10-29 16:45:33 -0300

    :sparkles: feat: Modelo base + gabarito e rotas

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import database from './database/database.js'
+import GabaritoRoutes from './routes/GabaritoRoutes'
 
 const app = express()
 app.use(express.json())
@@ -6,6 +8,10 @@ app.use(express.json())
 app.get('/healthcheck',(req,res)=>{
     res.send('Ok!')
 })
+
+//acessa as rotas Gabarito
+const gabaritoRoutes = new GabaritoRoutes(database)
+app.use('/gabaritos',gabaritoRoutes.routes())
 
 app.listen(3000,() => {
     console.log('Server rodando na porta 3000')
