06f93b2cc817844e67f76fac592de650dde7b104
IsabellaOshima <143272475+IsabellaOshima@users.noreply.github.com>
2023-10-29 15:31:19 -0300

    iniciei a estrutura do código

---
+++ src/database/database.js
@@ -0,0 +1,14 @@
+//importa todas as entidades criadas
+import Aluno from "../entities/Aluno.js"
+import Disciplina from "../entities/Disiciplina.js"
+import Gabarito from "../entities/Gabarito.js"
+import Nota from "../entities/Nota.js"
+
+//cria um banco de dados com as entidades
+const database = {
+    gabaritos: [
+        new Gabarito(1,1,[a,a,a,a,a],21/10/2023)
+    ]
+}
+
+export default database
