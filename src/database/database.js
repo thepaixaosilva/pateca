25be91a295a274da9dc20cc38eb6f0be35adbde5
Matheus Silva <72952802+matsilva03@users.noreply.github.com>
2023-10-29 21:36:37 -0300

    :bug: fix: variáveis em vez de strings

--- src/database/database.js
+++ src/database/database.js
@@ -7,7 +7,7 @@ import Nota from "../entities/Nota.js"
 //cria um banco de dados com as entidades
 const database = {
     gabaritos: [
-        new Gabarito(1,1,[a,a,a,a,a],21/10/2023)
+        new Gabarito(1,1,["a","a","a","a","a"],"21/10/2023")
     ]
 }
 
