2d3abee90d9fa923e7149a2ee88228234e4574ac
IsabellaOshima <oshima.isabella@gmail.com>
2023-11-07 20:25:21 -0300

    :bug: fix: respostas = variáveis, em vez de um vetor
    - database também foi renomeado para db.

--- src/database/database.js
+++ src/database/database.js
@@ -5,7 +5,7 @@ import Gabarito from "../entities/Gabarito.js"
 import Nota from "../entities/Nota.js"
 
 //cria um banco de dados com as entidades
-const database = {
+const db = {
     alunos: [
         new Aluno("1234567890123", "Fulano da Silva", "asdfasdfasdf"),
         new Aluno("0987654321098", "Cicrano Bastos", "qwerqwerqwerqwer")
@@ -15,11 +15,11 @@ const database = {
         new Disciplina("AG994", "Administração Geral", 1)
     ],
     gabarito: [
-        new Gabarito(1, 1, ["a", "a", "a", "a", "a"], "21/10/2023")
+        new Gabarito(1, 1, "a", "a", "a", "a", "a", "21/10/2023")
     ],
     nota: [
         new Nota(1, "1234567890123", "LP003", 1, 10)
     ]
 }
 
-export default database
+export default db
--- src/entities/Gabarito.js
+++ src/entities/Gabarito.js
@@ -1,6 +1,6 @@
 // Entidade gabarito.
 export default class Gabarito {
-    constructor (codGabarito, codMateria, [resp1, resp2, resp3, resp4, resp5], dataProva){
+    constructor (codGabarito, codMateria, resp1, resp2, resp3, resp4, resp5, dataProva){
         this.codGabarito = codGabarito
         this.codMateria = codMateria
         this.respostaProva = [resp1, resp2, resp3, resp4, resp5] // Será um array com as respotas de cada aluno em cada prova.
