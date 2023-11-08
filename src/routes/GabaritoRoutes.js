3aca7a22763e1a1091efbbf81c53c97da88356b2
IsabellaOshima <oshima.isabella@gmail.com>
2023-11-07 22:31:57 -0300

    :sparkles: feat(Gabarito): implementação parcial do POST

--- src/routes/GabaritoRoutes.js
+++ src/routes/GabaritoRoutes.js
@@ -17,21 +17,15 @@ export default class GabaritoRoutes {
             res.json(gabaritos)
         })
 
-        /*rota Create
-        router.post('/',(req,res) => {
-            const {codGabarito, resp1, resp2, resp3, resp4 , resp5, codMateria, dataProva} = req.body //extrai os dados que foram colocados no corpo da requisição
+        /*router.post('/',(req,res) => {
+            const novoGabarito = req.body
 
-            //validação:
-            if(!codGabarito || codGabarito == "") return res.status(400).json({erro: 'Código gabarito obrigatório'})
-            if(!codMateria || codMateria == "") return res.status(400).json({erro: 'Código matéria obrigatório'})
-            if(!dataProva || dataProva == "") return res.status(400).json({erro: 'Data da Prova obrigatória'})
-
-            const gabarito = new Gabarito(codGabarito, resp1, resp2, resp3, resp4 , resp5,codMateria, dataProva) //cria um novo gabarito
-            this.db.create(gabarito)//usando o método criado na BaseModel adiciona esse novo gabarito ao banco de dados
-            res.status(201).json(tarefa)
-        })
+            this.db.create(novoGabarito)
+            res.json(novoGabarito)
+        })*/
 
         //rota FindById
+        /*
         router.get('/:codGabarito',(req,res) => { //codGbarito é um parametro
             const {codGabarito} = req.params //extrai o codigo que foi colocado como parametro
 
