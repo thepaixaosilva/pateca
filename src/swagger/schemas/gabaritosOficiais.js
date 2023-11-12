7f56afde3ee8f2ea57b54cf7c98ba516aab403d1
IsabellaOshima <143272475+IsabellaOshima@users.noreply.github.com>
2023-11-12 13:03:22 -0300

    swagger gabarito oficial schemas

---
+++ src/swagger/schemas/gabaritosOficiais.js
@@ -0,0 +1,28 @@
+export const GabaritoOficial = {
+    type: "object",
+    required: ["id","respostaProva", "codDisciplina", "dataProva"],
+    properties: {
+        id: {
+            type: "number",
+            description: "ID do gabarito oficial",
+        },
+        respostaProva: {
+            type: "string", //array de strings
+            description: "Respostas da prova",
+        },
+        codDisciplina: {
+            type: "number",
+            description: "Código da disciplina",
+        },
+        dataProva: {
+            type: "string",
+            description: "Data da Prova",
+        },
+    },
+    example: {
+        id: 1,
+        respostaProva: [ "a", "b","c","d","e"],
+        codDisciplina: 1,
+        dataProva: "12/11/2023",
+    },
+}
