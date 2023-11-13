d8d94525d6ed8490075d78e3144a4893ad21ebd5
MATHEUS PAIXAO SILVA <matpaixaoo@outlook.com>
2023-11-13 18:53:41 -0300

    :books: docs(Aluno): documentação finalizada

--- src/swagger/paths/index.js
+++ src/swagger/paths/index.js
@@ -1,6 +1,9 @@
 import { GetPostGabaritoOficial, GetDeletePutGabaritoOficialById } from "./gabaritosOficiais.js";
+import { GetPostAluno, GetDeletePutAlunoById} from "./aluno.js"
 
 export default {
     '/gabaritoOficiais': GetPostGabaritoOficial,
-    '/gabaritoOficiais/{id}': GetDeletePutGabaritoOficialById
+    '/gabaritoOficiais/{id}': GetDeletePutGabaritoOficialById,
+    '/alunos': GetPostAluno,
+    '/alunos/{id}': GetDeletePutAlunoById
 }
