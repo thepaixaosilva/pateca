d8128d62f92b8afbf057884c1847c103d4540e74
Victoria Miki Fujii <fujiimikii@gmail.com>
2023-11-17 23:47:15 -0300

    :books: docs(GabaritoAluno): documentação das rotas novas

--- src/swagger/paths/index.js
+++ src/swagger/paths/index.js
@@ -1,13 +1,22 @@
-import { GetPostGabaritoOficial, GetDeletePutGabaritoOficialById } from "./gabaritosOficiais.js";
 import { GetPostAluno, GetDeletePutAlunoById, GetAlunoByName} from "./aluno.js"
 import { GetPostDisciplina, GetDeletePutDisciplinaById} from "./disciplina.js"
+import { GetPostGabaritoAluno, GetDeletePutGabaritoAlunoById, GetGabaritoAlunoByAlunoId, GetGabaritoAlunoByDisciplinaId, GetGabaritoAlunoByGabaritoOficialId } from "./gabaritoAluno.js"
+import { GetPostGabaritoOficial, GetDeletePutGabaritoOficialById } from "./gabaritosOficiais.js";
 
 export default {
-    '/gabaritoOficiais': GetPostGabaritoOficial,
-    '/gabaritoOficiais/{id}': GetDeletePutGabaritoOficialById,
     '/alunos': GetPostAluno,
     '/alunos/{id}': GetDeletePutAlunoById,
     '/alunos/nome/{name}': GetAlunoByName,
+
     '/disciplinas': GetPostDisciplina,
     '/disciplinas/{id}': GetDeletePutDisciplinaById,
+
+    '/gabaritoAlunos': GetPostGabaritoAluno,
+    '/gabaritoAlunos/{id}': GetDeletePutGabaritoAlunoById,
+    '/gabaritoAlunos/alunos/{id}': GetGabaritoAlunoByAlunoId,
+    '/gabaritoAlunos/disciplinas/{id}': GetGabaritoAlunoByDisciplinaId,
+    '/gabaritoAlunos/gabaritoOficiais/{id}': GetGabaritoAlunoByGabaritoOficialId,
+
+    '/gabaritoOficiais': GetPostGabaritoOficial,
+    '/gabaritoOficiais/{id}': GetDeletePutGabaritoOficialById,
 }
