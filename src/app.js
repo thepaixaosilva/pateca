00b2b7e297416427131e23360ebf289159f85d05
Matheus Silva <matpaixaoo@outlook.com>
2025-07-09 20:07:36 -0300

    fix: adjusted to regex & adjusted to httpStatus.status

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
-const xss = require('xss-clean');
-// const cors = require('cors');
+const cors = require('cors');
 const compression = require('compression');
 const morgan = require('morgan');
 const httpStatus = require('http-status');
 const { config } = require('./config/environment.config');
 const { errorConverter, errorHandler } = require('./middlewares/error');
+const routes = require('./routes');
 const ApiError = require('./utils/ApiError');
 
 const app = express();
@@ -20,16 +20,16 @@ app.use(helmet());
 
 app.use(express.json());
 
-app.use(xss());
-
 app.use(compression());
 
-// app.use(cors());
-// app.options("*", cors());
+app.options(/(.*)/, cors());
+app.use(cors());
+
+app.use('/', routes);
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
-  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+  next(new ApiError(httpStatus.status.NOT_FOUND, 'Not found'));
 });
 
 // convert error to ApiError, if needed
--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -5,14 +5,15 @@ const ApiError = require('../utils/ApiError');
 const { config } = require('../config/environment.config');
 
 const errorConverter = (err, req, res, next) => {
+  console.error(err);
   let error = err;
   if (!(error instanceof ApiError)) {
     const statusCode =
       error.statusCode || error instanceof DatabaseError
-        ? httpStatus.BAD_REQUEST
-        : httpStatus.INTERNAL_SERVER_ERROR;
+        ? httpStatus.status.BAD_REQUEST
+        : httpStatus.status.INTERNAL_SERVER_ERROR;
 
-    const message = error.message || httpStatus[statusCode];
+    const message = error.message || httpStatus.message[statusCode];
     error = new ApiError(statusCode, message, false, err.stack);
   }
   next(error);
@@ -22,15 +23,15 @@ const errorConverter = (err, req, res, next) => {
 const errorHandler = (err, req, res, next) => {
   let { statusCode, message } = err;
   if (config.env === 'production' && !err.isOperational) {
-    statusCode = httpStatus.INTERNAL_SERVER_ERROR;
-    message = httpStatus[httpStatus.INTERNAL_SERVER_ERROR];
+    statusCode = httpStatus.status.INTERNAL_SERVER_ERROR;
+    message = httpStatus[httpStatus.status.INTERNAL_SERVER_ERROR];
   }
 
   res.locals.errorMessage = err.message;
 
   const response = {
     code: statusCode,
-    message,
+    error: message,
     ...(config.env === 'development' && { stack: err.stack }),
   };
 
